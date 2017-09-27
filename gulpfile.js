e756215c0a822971e8dbc97aa1a574899b4688fa
samilHome <denis@gmail.com>
2017-09-27 23:06:04 +0000

    почти доделал мини карточку

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp      = require('gulp'), // Подключаем Gulp
-    sass        = require('gulp-sass'), //Подключаем Sass пакет,
+  //  sass        = require('gulp-sass'), //Подключаем Sass пакет,
     browserSync = require('browser-sync'); // Подключаем Browser Sync
 var csso = require ('gulp-csso');
 var concat = require('gulp-concat');
@@ -9,12 +9,12 @@ const autoprefixer = require('gulp-autoprefixer');
 var less = require('gulp-less');
 
 
-gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('app/sass/**/*.scss') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-});
+//gulp.task('sass', function(){ // Создаем таск Sass
+  //  return gulp.src('app/sass/**/*.scss') // Берем источник
+    //    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+      //  .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        //.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+//});
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
